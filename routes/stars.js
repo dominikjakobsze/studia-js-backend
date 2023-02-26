99128f0947c7b05ec61007d0d4df2db6f59f2444
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-02-27 00:14:18 +0100

    file upload done

--- routes/stars.js
+++ routes/stars.js
@@ -1,9 +1,33 @@
 import express from "express";
 import multer from "multer";
+import path from "path";
+import {fileURLToPath} from 'url';
 import * as fs from "fs";
 
 const router = express.Router();
-const upload = multer();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "../uploads/"));
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+    limits: {
+        fileSize: 10 * 1024 * 1024, // 10 MB in bytes
+    },
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype !== "image/png" && file.mimetype !== "image/jpeg") {
+            return cb(new Error("Only png and jpeg files are allowed"));
+        }
+        cb(null, true);
+    }
+});
+
+const upload = multer({storage: storage});
 
 router.get("/", (req, res) => {
     res.send("Hello World!");
@@ -21,16 +45,12 @@ router.post("/", upload.single("image"), (req, res) => {
     if (file === undefined) {
         res.status(400).send({message: "Image is required"});
     }
-    if (file.size > 1000000) {
-        res.status(400).send({message: "Image is too big"});
-    }
-    fs.rename
     res.send({
         message: "You have added a new start!",
         data: {
             name,
             article,
-            imageUrl: file,
+            imageUrl: file
         },
     });
 });
