12e57832d5dd6ab01ccc9366f725f5b5dfa8a0b7
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-02-26 15:19:14 +0100

    readme

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = 5200;
 
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    console.log('route: /');
+    res.send('Hello World!');
+});
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
