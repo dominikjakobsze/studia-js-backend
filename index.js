d5dea131701191efd634eb51e06eb5bf79e75c77
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-02-26 15:25:25 +0100

    users routes

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import usersRouter from "./routes/users.js";
 
 const app = express();
 const PORT = 5200;
 
 app.use(bodyParser.json());
+app.use('/users', usersRouter);
 
-app.get('/', (req, res) => {
-    console.log('route: /');
-    res.send('Hello World!');
-});
 app.listen(PORT, () => {
     console.log(`Server running on http://127.0.0.1:${PORT}`);
 });
---
+++ routes/users.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const usersRouter = express.Router();
+
+usersRouter.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+export default usersRouter;
